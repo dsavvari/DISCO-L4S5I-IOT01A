22830213ef2ec689580b1702cfff2595f2007ec2
Dimitris Savvaris <dimitris@skoposltd.co.uk>
2022-10-18 21:03:20 +0100

    Non functional changes - indentation & code formatting

--- mbed-os-sampling-sensors-basic/main.cpp
+++ mbed-os-sampling-sensors-basic/main.cpp
@@ -7,7 +7,6 @@
 #include "stm32l4s5i_iot01_magneto.h"
 #include "stm32l4s5i_iot01_psensor.h"
 
-
 static BufferedSerial serial_port(USBTX, USBRX, 9600);
 static DigitalOut led(LED1);
 static DigitalOut led2(LED2);
@@ -35,42 +34,32 @@ static void readoutTick()
 
 static void readSensors()
 {
-    printf("Sensor values:\n");
-
     float temp = BSP_TSENSOR_ReadTemp();
-    printf("* Temperature: %f C, %f F, %f K\n",
-      temp,
-      0,//convertCelsiusToFahrenheit(temp),
-      0); //convertCelsiusToKelvin(temp));
-
-
     float humid = BSP_HSENSOR_ReadHumidity();
-    printf("* Humidity: %f \n", humid );
-
     float pressure = BSP_PSENSOR_ReadPressure();
-    printf("* Pressure: %f \n", pressure );
-
     int16_t magnetoXYZ[3] ={0}; 
     BSP_MAGNETO_GetXYZ(magnetoXYZ);
-    printf("* Magneto - X: %d Y: %d Z: %d \n", magnetoXYZ[0],magnetoXYZ[1], magnetoXYZ[2]);
-
     float gyroXYZ[3] = {0.0}; 
     BSP_GYRO_GetXYZ(gyroXYZ);
-    printf("* Gyro - X: %f Y: %f Z: %f \n", gyroXYZ[0], gyroXYZ[1], gyroXYZ[2]);
- 
     int16_t acceleroXYZ[3] ={0}; 
     BSP_ACCELERO_AccGetXYZ(acceleroXYZ);
-    printf("* Accelerometer - X: %d Y: %d Z: %d \n", acceleroXYZ[0], acceleroXYZ[1], acceleroXYZ[2]);
 
+    printf("Sensor values:\n");
+    printf("* Temperature: %f C\n",temp);
+    printf("* Humidity: %f \n", humid );
+    printf("* Pressure: %f \n", pressure );
+    printf("* Magneto - X: %d Y: %d Z: %d \n", magnetoXYZ[0],magnetoXYZ[1], magnetoXYZ[2]);
+    printf("* Gyro - X: %f Y: %f Z: %f \n", gyroXYZ[0], gyroXYZ[1], gyroXYZ[2]);
+    printf("* Accelerometer - X: %d Y: %d Z: %d \n", acceleroXYZ[0], acceleroXYZ[1], acceleroXYZ[2]);
 }
 static void initialiseSensors()
 {
-  BSP_TSENSOR_Init();
-  BSP_HSENSOR_Init();
-  BSP_PSENSOR_Init();
-  BSP_MAGNETO_Init();
-  BSP_ACCELERO_Init();
-  BSP_GYRO_Init();
+    BSP_TSENSOR_Init();
+    BSP_HSENSOR_Init();
+    BSP_PSENSOR_Init();
+    BSP_MAGNETO_Init();
+    BSP_ACCELERO_Init();
+    BSP_GYRO_Init();
 }
 
 int main()
@@ -81,12 +70,12 @@ int main()
     readoutTicker.attach(&readoutTick, 3s);
     initialiseSensors();
     
+    /*forever*/
     while (true) {
         sleep();
         if (shouldReadSensors) {
-        readSensors();
-        shouldReadSensors = false;
+            readSensors();
+            shouldReadSensors = false;
+        }
     }
 }
-}
-
