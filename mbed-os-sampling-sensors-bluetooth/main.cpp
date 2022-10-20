4ce368015fdc41c39c4780d07bdabc61197a34af
Dimitris Savvaris <dimitris@skoposltd.co.uk>
2022-10-20 22:34:55 +0100

    Refactored main, added support for input env. char

--- mbed-os-sampling-sensors-bluetooth/main.cpp
+++ mbed-os-sampling-sensors-bluetooth/main.cpp
@@ -8,103 +8,114 @@
 #include "stm32l4s5i_iot01_psensor.h"
 #include "ble/BLE.h"
 #include "ble_process.h"
+#include <initializer_list>
 
 class Lab3ServerProcess : public BLEProcess {
 public:
-  Lab3ServerProcess(events::EventQueue &event_queue, BLE &ble_interface)
-      : BLEProcess(event_queue, ble_interface) {}
+    Lab3ServerProcess(events::EventQueue &event_queue, BLE &ble_interface): BLEProcess(event_queue, ble_interface) {
+    }
 
-  const char *get_device_name() override {
-    static const char name[] = "Lab 3 Server";
-    return name;
-  }
+    const char *get_device_name() override {
+        static const char name[] = "Lab 3 Server";
+        return name;
+    }
 };
 
 class SensorReadingCharacteristic : public GattCharacteristic {
 public:
-  SensorReadingCharacteristic(const UUID &uuid)
-      : GattCharacteristic(
-            uuid, (uint8_t *)&stringRepr_, sizeof(stringRepr_),
-            sizeof(stringRepr_),
-            GattCharacteristic::BLE_GATT_CHAR_PROPERTIES_READ |
-                GattCharacteristic::BLE_GATT_CHAR_PROPERTIES_NOTIFY),
-        internalValue_(0) {
-    updateStringRepr();
-  }
+    SensorReadingCharacteristic(const UUID &uuid): GattCharacteristic(uuid, (uint8_t *)&stringRepr_, sizeof(stringRepr_),
+    sizeof(stringRepr_), GattCharacteristic::BLE_GATT_CHAR_PROPERTIES_READ | GattCharacteristic::BLE_GATT_CHAR_PROPERTIES_NOTIFY), 
+    internalValue_(0) {
+        updateStringRepr();
+    }
 
-  void updateValue(BLE &ble, float newValue) {
-    internalValue_ = newValue;
-    updateStringRepr();
+    void updateValue(BLE &ble, float newValue) {
+        internalValue_ = newValue;
+        updateStringRepr();
 
-    ble.gattServer().write(getValueHandle(), (uint8_t *)&stringRepr_,
-                           sizeof(stringRepr_));
-  }
+        ble.gattServer().write(getValueHandle(), (uint8_t *)&stringRepr_, sizeof(stringRepr_));
+    }
 
 private:
-  float internalValue_;
-  uint8_t stringRepr_[16];
+    float internalValue_;
+    uint8_t stringRepr_[16];
 
-  void updateStringRepr() {
-    sprintf((char *)stringRepr_, "%f", internalValue_);
-  }
+    void updateStringRepr() {
+        sprintf((char *)stringRepr_, "%f", internalValue_);
+    }
 };
 
 
 class Lab3Server : ble::GattServer::EventHandler {
 public:
-  Lab3Server()
-      : temperature_(GattCharacteristic::UUID_TEMPERATURE_CHAR),
-        humidity_(GattCharacteristic::UUID_HUMIDITY_CHAR),
-        pressure_(GattCharacteristic::UUID_PRESSURE_CHAR) {}
-        
-  ~Lab3Server() {}
+    Lab3Server() :  temperature_(GattCharacteristic::UUID_TEMPERATURE_CHAR),
+                    humidity_(GattCharacteristic::UUID_HUMIDITY_CHAR),
+                    pressure_(GattCharacteristic::UUID_PRESSURE_CHAR), 
+                    led_(LED1), 
+                    ledValue_(0), 
+                    ledChar_(0xA000, &ledValue_) {
 
-  void start(BLE &ble, events::EventQueue &event_queue) {
-    const UUID uuid = GattService::UUID_ENVIRONMENTAL_SERVICE;
+        initialiseSensors();
+    }
+    virtual ~Lab3Server() {}
 
-    GattCharacteristic *charTable[] = {&temperature_, &humidity_, &pressure_};
+    void start(BLE &ble, events::EventQueue &event_queue) {
+        const UUID uuid = GattService::UUID_ENVIRONMENTAL_SERVICE;
+        GattCharacteristic *charTable[] = {&temperature_, &humidity_, &pressure_, &ledChar_};
+        GattService sensorService(uuid, charTable,sizeof(charTable) / sizeof(charTable[0]));
 
-    GattService sensorService(uuid, charTable,
-                              sizeof(charTable) / sizeof(charTable[0]));
+        ble.gattServer().addService(sensorService);
+        ble.gattServer().setEventHandler(this);
 
-    ble.gattServer().addService(sensorService);
-    ble.gattServer().setEventHandler(this);
+        event_queue.call_every(5000ms, [this, &ble] { updateSensors(ble); });
 
-    event_queue.call_every(1000ms, [this, &ble] { updateSensors(ble); });
+        printf("Service started.\n");
 
-    printf("Service started.\n");
-  }
+    }
 
-  void updateSensors(BLE &ble) {
-  printf("Updating sensors...\n");
+    virtual void onDataWritten(const GattWriteCallbackParams &params) override {
+        if ((params.handle == ledChar_.getValueHandle()) && (params.len == 1)) {
+        printf("New LED value: %x\r\n", *(params.data));
+        ledValue_ = *(params.data);
+        led_ = ledValue_;
+        }
+    }
 
-  temperature_.updateValue(ble, BSP_TSENSOR_ReadTemp());
-  humidity_.updateValue(ble, BSP_HSENSOR_ReadHumidity());
-  pressure_.updateValue(ble, BSP_PSENSOR_ReadPressure());
-}
+    void initialiseSensors() {
+        BSP_TSENSOR_Init();
+        BSP_HSENSOR_Init();
+        BSP_PSENSOR_Init();
+    }
+
+
+    void updateSensors(BLE &ble) {
+
+        printf("Updating sensors...\n");
+        float temp = BSP_TSENSOR_ReadTemp();
+        float humid = BSP_HSENSOR_ReadHumidity();
+        float pressure = BSP_PSENSOR_ReadPressure();
+        printf("Sensor values - * Temperature: %f C * Humidity: %f  * Pressure: %f  \n",temp, humid, pressure );
+        temperature_.updateValue(ble, temp);
+        humidity_.updateValue(ble, pressure);
+        pressure_.updateValue(ble, pressure);
+    }
 
 private:
-  SensorReadingCharacteristic temperature_;
-  SensorReadingCharacteristic humidity_;
-  SensorReadingCharacteristic pressure_;
-
+    SensorReadingCharacteristic temperature_;
+    SensorReadingCharacteristic humidity_;
+    SensorReadingCharacteristic pressure_;
+    DigitalOut led_;
+    uint8_t ledValue_;
+    ReadWriteGattCharacteristic<uint8_t> ledChar_;
 };
 
 
 static EventQueue event_queue(10 * EVENTS_EVENT_SIZE);
-static void initialiseSensors() {
-  BSP_TSENSOR_Init();
-  BSP_HSENSOR_Init();
-  BSP_PSENSOR_Init();
-}
 
 
 int main() {
 
-  initialiseSensors();
   BLE &ble = BLE::Instance();
-
-  printf("Lab 3 - BLE");
 
   Lab3ServerProcess ble_process(event_queue, ble);
   Lab3Server server;
