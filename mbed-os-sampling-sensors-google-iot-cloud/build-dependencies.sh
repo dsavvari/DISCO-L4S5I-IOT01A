efb31c5dc0f6c98a5f9de5b90f8dac63ddc419a4
Dimitris Savvaris <dimitris@skoposltd.co.uk>
2022-11-07 15:18:00 +0000

    More dependencies redefinitions

--- mbed-os-sampling-sensors-google-iot-cloud/build-dependencies.sh
+++ mbed-os-sampling-sensors-google-iot-cloud/build-dependencies.sh
@@ -1,11 +1,13 @@
 PRIVATE_KEYS_DIR="private"
 MBED_OS_DIR="mbed-os"
 GOOGLE_IOT_DIR="mbed-client-for-google-iot-cloud"
+GOOGLE_IOT_SDK_DIR="mbed-client-for-google-iot-cloud/iot-device-sdk-embedded-c"
+
 NTP_CLIENT_DIR="ntp-client"
 
 if [ -d "$MBED_OS_DIR" ];
 then
-    echo "[$MBED_OS_DIR] exist.[OK]"
+	echo "[$MBED_OS_DIR] exist.[OK]"
 else
 	git clone https://github.com/ARMmbed/mbed-os.git mbed-os
 	cd mbed-os
@@ -17,7 +19,7 @@ fi
 
 if [ -d "$GOOGLE_IOT_DIR" ];
 then
-    echo "[$GOOGLE_IOT_DIR] exist.[OK]"
+	echo "[$GOOGLE_IOT_DIR] exist.[OK]"
 else
 	git clone https://github.com/ARMmbed/mbed-client-for-google-iot-cloud
 	cd mbed-client-for-google-iot-cloud
@@ -25,9 +27,20 @@ else
 	cd ..
 fi
 
+if [ -d "$GOOGLE_IOT_SDK_DIR" ];
+then
+	echo "[$GOOGLE_IOT_SDK_DIR] exist.[OK]"
+else
+	cd "$GOOGLE_IOT_DIR" 
+	git clone https://github.com/GoogleCloudPlatform/iot-device-sdk-embedded-c.git/
+	cd iot-device-sdk-embedded-c
+	git checkout 32b03168
+	cd ../../
+fi
+
 if [ -d "$NTP_CLIENT_DIR" ];
 then
-    echo "[$NTP_CLIENT_DIR] exist.[OK]"
+	echo "[$NTP_CLIENT_DIR] exist.[OK]"
 else
 	git clone https://github.com/ARMmbed/ntp-client.git
 	cd ntp-client
@@ -37,9 +50,9 @@ fi
 
 if [ -d "$PRIVATE_KEYS_DIR" ];
 then
-# create the private keys and update the following files
-    cp private/mbed_app.json .
-    cp private/google_cloud_credentials.h .
+	# create the private keys and update the following files
+	cp private/mbed_app.json .
+	cp private/google_cloud_credentials.h .
 else
-    echo "Error [$PRIVATE_KEYS_DIR] don't exist. Please create the secret keys first"
+	echo "Error [$PRIVATE_KEYS_DIR] don't exist. Please create the secret keys first"
 fi
