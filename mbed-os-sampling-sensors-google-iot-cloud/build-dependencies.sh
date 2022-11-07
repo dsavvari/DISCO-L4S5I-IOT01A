114bfe21af609392859f866df4e2bdd1a6aaf125
Dimitris Savvaris <dimitris@skoposltd.co.uk>
2022-11-07 12:21:33 +0000

    Added example of IOT over google cloud

---
+++ mbed-os-sampling-sensors-google-iot-cloud/build-dependencies.sh
@@ -0,0 +1,20 @@
+git clone https://github.com/ARMmbed/mbed-os.git mbed-os
+cd mbed-os
+git checkout mbed-os-6.16.0-rc1
+git apply --check ../mbed_lib.json.patch
+git apply  ../mbed_lib.json.patch
+
+cd ..
+git clone https://github.com/ARMmbed/mbed-client-for-google-iot-cloud
+cd mbed-client-for-google-iot-cloud
+git checkout cbf19de
+
+cd ..
+git clone https://github.com/ARMmbed/ntp-client.git
+cd ntp-client
+git checkout e919cfb
+
+# create the private keys and update the following files
+#cd..
+#cp private/mbed_app.json .
+#cp private/google_cloud_credentials.h .
