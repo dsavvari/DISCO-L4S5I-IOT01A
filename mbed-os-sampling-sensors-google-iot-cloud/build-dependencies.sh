95e5fad258ca3be64b1a7cc1b007669753a0ac83
Dimitris Savvaris <dimitris@skoposltd.co.uk>
2022-11-07 15:51:38 +0000

    wifi module dependency added

--- mbed-os-sampling-sensors-google-iot-cloud/build-dependencies.sh
+++ mbed-os-sampling-sensors-google-iot-cloud/build-dependencies.sh
@@ -2,7 +2,7 @@ PRIVATE_KEYS_DIR="private"
 MBED_OS_DIR="mbed-os"
 GOOGLE_IOT_DIR="mbed-client-for-google-iot-cloud"
 GOOGLE_IOT_SDK_DIR="mbed-client-for-google-iot-cloud/iot-device-sdk-embedded-c"
-
+WIFI_DIR="wifi-ism43362"
 NTP_CLIENT_DIR="ntp-client"
 
 if [ -d "$MBED_OS_DIR" ];
@@ -10,7 +10,7 @@ then
 	echo "[$MBED_OS_DIR] exist.[OK]"
 else
 	git clone https://github.com/ARMmbed/mbed-os.git mbed-os
-	cd mbed-os
+	cd "$MBED_OS_DIR"
 	git checkout mbed-os-6.16.0-rc1
 	git apply --check ../mbed_lib.json.patch
 	git apply  ../mbed_lib.json.patch
@@ -22,7 +22,7 @@ then
 	echo "[$GOOGLE_IOT_DIR] exist.[OK]"
 else
 	git clone https://github.com/ARMmbed/mbed-client-for-google-iot-cloud
-	cd mbed-client-for-google-iot-cloud
+	cd "$GOOGLE_IOT_DIR"
 	git checkout cbf19de
 	cd ..
 fi
@@ -31,7 +31,7 @@ if [ -d "$GOOGLE_IOT_SDK_DIR" ];
 then
 	echo "[$GOOGLE_IOT_SDK_DIR] exist.[OK]"
 else
-	cd "$GOOGLE_IOT_DIR" 
+	cd "$GOOGLE_IOT_DIR"
 	git clone https://github.com/GoogleCloudPlatform/iot-device-sdk-embedded-c.git/
 	cd iot-device-sdk-embedded-c
 	git checkout 32b03168
@@ -43,16 +43,26 @@ then
 	echo "[$NTP_CLIENT_DIR] exist.[OK]"
 else
 	git clone https://github.com/ARMmbed/ntp-client.git
-	cd ntp-client
+	cd "$NTP_CLIENT_DIR"
 	git checkout e919cfb
+	cd ..
+fi
+
+if [ -d "$WIFI_DIR" ];
+then
+	echo "[$WIFI_DIR] exist.[OK]"
+else
+	git clone https://github.com/ARMmbed/wifi-ism43362/
+	cd "$WIFI_DIR"
+	git checkout 3813a4bb8623cc9b0
 	cd ..
 fi
 
 if [ -d "$PRIVATE_KEYS_DIR" ];
 then
 	# create the private keys and update the following files
-	cp private/mbed_app.json .
-	cp private/google_cloud_credentials.h .
+	cp $PRIVATE_KEYS_DIR/mbed_app.json .
+	cp $PRIVATE_KEYS_DIR/google_cloud_credentials.h .
 else
 	echo "Error [$PRIVATE_KEYS_DIR] don't exist. Please create the secret keys first"
 fi
